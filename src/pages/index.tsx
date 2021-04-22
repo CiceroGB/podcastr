9d90b1ce7b8f67652614c7453182ff2be96951fb
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-04-21 23:38:33 -0300

    api hadle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,42 @@
+import { GetStaticProps } from 'next';
 
-export default function Home() {
+import { api } from '../services/api';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>index</h1>
+    <div>
+
+
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes',{
+    params:{
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+
+    }
+  })
+
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
