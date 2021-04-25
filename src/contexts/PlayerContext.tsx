47abec83834795b28bfa51a6b48432ecf1082195
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-04-25 17:26:36 -0300

    export useContext inside COntext

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useContext } from 'react';
 
 type Episode = {
     id: string;
@@ -121,4 +121,8 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
         </PlayerContext.Provider>
     )
 }
+
+export const usePlayer = () => {
+    return useContext(PlayerContext)
+  }
 
